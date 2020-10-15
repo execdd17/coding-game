b3f27cc858db2767b812a6936c83776d38ae030b
Alexander Vanadio <execdd17@gmail.com>
2020-10-14 21:05:34 -0400

    added another puzzle; ruby again

---
+++ ruby/no-spoon-eps1/No Spoon.rb
@@ -0,0 +1,60 @@
+# https://www.codingame.com/training/medium/there-is-no-spoon-episode-1
+
+STDOUT.sync = true # DO NOT REMOVE
+# Don't let the machines win. You are humanity's last hope...
+
+width = gets.to_i # the number of cells on the X axis
+height = gets.to_i # the number of cells on the Y axis
+
+m = Array.new(height)
+m_transposed = Array.new(width) { Array.new(height) }
+
+class Point
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+
+  attr_reader :x, :y
+end
+
+def find_right(row_i, col_i, matrix)
+  col_start_index = [col_i + 1, matrix[row_i].length].min
+  search_space = matrix[row_i][col_start_index..-1]
+  closest_index = search_space.find_index { |cell| cell == "0" }
+
+  # STDERR.puts("matrix:#{matrix} #{col_start_index} #{search_space}, #{closest_index}")
+
+  if closest_index
+    Point.new(row_i, col_start_index + closest_index)
+  else
+    Point.new(-1, -1)
+  end
+end
+
+height.times do |i|
+  line = gets.chomp # width characters, each either 0 or .
+  m[i] = line.split("")
+end
+
+height.times do |row|
+  width.times do |col|
+    STDERR.print(m[row][col])
+    m_transposed[col][row] = m[row][col]
+  end
+  STDERR.puts
+end
+
+STDERR.puts("m: #{m}")
+STDERR.puts("m_transposed: #{m_transposed}")
+
+height.times do |row|
+  width.times do |col|
+    if m[row][col] == "0"
+      STDERR.puts("processing row:#{row} col:#{col} (#{col},#{row})")
+      right_point = find_right(row, col, m)
+      down_point = find_right(col, row, m_transposed)
+      puts "#{col} #{row} #{right_point.y} #{right_point.x} #{down_point.x} #{down_point.y}"
+    end
+  end
+end
